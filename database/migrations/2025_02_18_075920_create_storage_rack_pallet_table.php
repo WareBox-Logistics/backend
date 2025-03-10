0f0d5c8fe10e990195c0129a1858d9e24719dc10
thebancer <0321101710@ut-tijuana.edu.mx>
2025-03-09 21:29:36 -0700

    se agrego trigger

--- database/migrations/2025_02_18_075920_create_storage_rack_pallet_table.php
+++ database/migrations/2025_02_18_075920_create_storage_rack_pallet_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -28,6 +29,27 @@ return new class extends Migration
             $table->primary(['pallet', 'rack']);
             $table->unique(['rack','position','level']);
         });
+
+        DB::unprepared("
+        CREATE OR REPLACE FUNCTION check_level_validity()
+        RETURNS TRIGGER AS $$
+        DECLARE
+            max_level INTEGER;
+        BEGIN
+            SELECT levels INTO max_level FROM rack WHERE id = NEW.rack_id;
+            IF NEW.level > max_level THEN
+                RAISE EXCEPTION 'Nivel % es mayor que el máximo permitido % para el rack %', NEW.level, max_level, NEW.rack_id;
+            END IF;
+            RETURN NEW;
+        END;
+        $$ LANGUAGE plpgsql;
+
+        CREATE TRIGGER validate_level_before_insert
+        BEFORE INSERT OR UPDATE ON storage_rack_pallets
+        FOR EACH ROW
+        EXECUTE FUNCTION check_level_validity();
+    ");
+
     }
 
     /**
