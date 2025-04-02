68da7a66d74d545a109f37cb29654f0b5b5c10da
thebancer <0321101710@ut-tijuana.edu.mx>
2025-04-01 18:26:08 -0700

    mi push a back| endpoints support| reports|issue|problem

--- app/Http/Controllers/SupportController.php
+++ app/Http/Controllers/SupportController.php
@@ -10,8 +10,10 @@ class SupportController extends Controller
 {
     public function index(){
         try{
+            $support = Support::with(['issue','operator'])->get();
+
             return response()->json([
-                'data' => Support::all()
+                'supports' => $support
             ]);
         }catch(Exception $e){
             return response()->json($e);
@@ -37,8 +39,51 @@ class SupportController extends Controller
                 'status' => 'required||in:WIP,DONE,WAIT',
                 'operator' => 'required|exists:employee,id',
             ]);
+            return response()->json([
+                'supports' => Support::create($validatedData)
+            ]);
         }catch(Exception $e){
             return response()->json($e);
+        }
+    }
+
+    public function update(Request $request, $id){
+        try{
+            $support = Support::findOrFail($id);
+
+            $fields = $request->validate([
+                'description' => 'required|string',
+                'issue' => 'required|exists:issue,id',
+                'status' => 'required||in:WIP,DONE,WAIT',
+                'operator' => 'required|exists:employee,id',
+            ]);
+            $support->update($fields);
+
+            return response()->json([
+                'supports' => $support
+            ], 200);
+
+        }catch(Exception $e){
+            return response()->json([
+                'error' => 'Error updating support',
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function destroy($id){
+        try{
+            $support = Support::findOrFaild($id);
+            $support->delete();
+
+            return response()->json([
+                'supports' => $support
+            ], 200);
+        }catch(Exception $e){
+            return response()->json([
+                'error' => 'error deleting support',
+                'message' => $e->getMessage()
+            ]);
         }
     }
 }
--- app/Models/Report.php
+++ app/Models/Report.php
@@ -21,7 +21,7 @@ class Report extends Model
     ];
 
     public function issues(){
-        return $this->hasMany(Issue::class);
+        return $this->hasMany(Issue::class,'report');
     }
 
     public function problem(){
